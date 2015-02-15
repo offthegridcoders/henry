ce5d68c9f073cd83b4bfdb8f7888c24d26beb3d1
Stephen <steveyoung359@gmail.com>
2015-02-15 17:22:20 -0500

    updated gulp and structure

--- gulpfile.js
+++ gulpfile.js
@@ -18,65 +18,64 @@ var paths = {
   assets: 'src/assets/**/*',
   vendor: 'src/scss/vendor/*.css',
   css: 'src/css/*.css',
-  html: 'src/**/*.html',
   js: 'src/js/**/*.js',
-  templates: 'src/templates/_*.html',
+  templates: 'src/templates/**/*.html',
+  partials: 'src/partials/**/*.html',
   distHTML: 'dist/**/*.html'
 };
 
-gulp.task('default', ['sass', 'make', 'clean-up'], function() {
+gulp.task('default', ['sass', 'make'], function() {
   return gulp.watch([
       paths.scss,
       paths.assets,
-      paths.html,
       paths.js,
       paths.templates
     ], ['default']);
 });
 
-gulp.task('build', ['sass', 'make', 'clean-up', 'image-compress'], function() {});
+gulp.task('build', ['make', 'image-compress'], function() {});
 
 // compiles scss then minifies and uglifies all css files
 // including vendor files and scss files
 
   gulp.task('sass', ['minify-css']);
 
-  gulp.task('compile-sass', function() {
+  gulp.task('compile-sass', ['clear'], function() {
     return gulp.src(paths.scss)
       .pipe(scsslint())
       .pipe(sass())
-      .pipe(gulp.dest('./src/scss'));
+      .pipe(gulp.dest('./dist/css'));
   });
 
   gulp.task('concat-css', ['compile-sass'], function () {
-    return gulp.src(['./src/scss/*.css', paths.vendor])
+    return gulp.src(['./dist/css/*.css', paths.vendor])
       .pipe(concatCss('main.css'))
-      .pipe(gulp.dest('./src/css'));
+      .pipe(gulp.dest('./dist/css'));
   });
 
   gulp.task('minify-css', ['concat-css'], function() {
-    return gulp.src('./src/css/main.css')
+    return gulp.src('./dist/css/main.css')
       .pipe(minifyCSS())
-      .pipe(gulp.dest('./src/css'))
+      .pipe(gulp.dest('./dist/css/'))
   });
 
 // builds complete site and exports into
 // dist folder then runs cleanup
 
-  gulp.task('make', ['clear', 'useref', 'img-copy'], function() {
+  gulp.task('make', ['clear', 'sass', 'useref', 'img-copy', 'compress'], function() {
     return gulp.src(paths.distHTML)
     .pipe(fileinclude())
     .pipe(gulp.dest('./dist'));
   });
 
-  gulp.task('clear', ['sass'], function () {
+  gulp.task('clear', function () {
     return gulp.src('dist', {read: false})
       .pipe(clean());
   });
 
   gulp.task('useref', ['fileinclude'], function () {
     var assets = useref.assets();
-    return gulp.src(paths.html)
+    return gulp.src(paths.templates)
       .pipe(assets)
       .pipe(assets.restore())
       .pipe(useref())
@@ -94,12 +93,13 @@ gulp.task('build', ['sass', 'make', 'clean-up', 'image-compress'], function() {}
     .pipe(gulp.dest('./dist/assets'));
   })
 
-  gulp.task('clean-up', ['make'], function() {
-    return gulp.src(['./dist/partials/'], {read: false})
-      .pipe(clean())
+  gulp.task('compress', ['useref'], function() {
+    return gulp.src('./src/js/scripts.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('./dist/js/'))
   });
 
-  gulp.task('image-compress', ['clean-up'], function () {
+  gulp.task('image-compress', function () {
     return gulp.src('./dist/assets/**/*.*')
       .pipe(imagemin({
         progressive: true,
